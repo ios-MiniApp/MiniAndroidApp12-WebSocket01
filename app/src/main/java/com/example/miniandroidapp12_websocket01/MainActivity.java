fd2599d0eb3afb2f04934272196fa588707ce43c
Kota <maedakoutaa@gmail.com>
2022-11-29 22:30:04 +0900

    ActivityからTextView変更

--- app/src/main/java/com/example/miniandroidapp12_websocket01/MainActivity.java
+++ app/src/main/java/com/example/miniandroidapp12_websocket01/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.miniandroidapp12_websocket01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello World!");
     }
 }
