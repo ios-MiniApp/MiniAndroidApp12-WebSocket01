871803213b4c198e7cc0c4e311755bc9a5bc3ab2
Kota <maedakoutaa@gmail.com>
2022-11-30 17:14:20 +0900

    仮完成

--- app/src/main/java/com/example/miniandroidapp12_websocket01/MainActivity.java
+++ app/src/main/java/com/example/miniandroidapp12_websocket01/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.miniandroidapp12_websocket01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +26,55 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText("Hello World!");
+
+        WebSocketClient webSocketClient = new WebSocketClient();
+        webSocketClient.send("Hello from Android");
     }
+}
+
+class WebSocketClient extends WebSocketListener {
+    private WebSocket webSocket;
+
+    // コンストラクタ
+    public WebSocketClient() {
+        OkHttpClient client = new OkHttpClient();
+
+        // 接続先のエンドポイント
+        // localhostとか127.0.0.1ではないことに注意
+        Request.Builder request = new Request.Builder();
+        request.url("ws://10.0.2.2:8080");
+
+        webSocket = client.newWebSocket(request.build(), this);
+    }
+
+    public void send(String message) {
+        webSocket.send(message);
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, Response response) {
+        System.out.println("WebSocket opened successfully");
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String text) {
+        System.out.println("WebSocket opened successfully");
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, ByteString bytes) {
+        System.out.println("WebSocket opened successfully");
+    }
+
+    @Override
+    public void onClosing(WebSocket webSocket, int code, String reason) {
+        webSocket.close(1000, null);
+        System.out.println("WebSocket opened successfully");
+    }
+
+    @Override
+    public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+        System.out.println("WebSocket opened successfully");
+    }
+
 }
